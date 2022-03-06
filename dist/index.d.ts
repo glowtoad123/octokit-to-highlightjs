5541a2482019924879e64dd23b53a8cdcd7755b7
glowtoad123 <alonzo6546@outlook.com>
2022-03-05 19:09:49 -0500

    added license

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -21,7 +21,7 @@ declare class oth {
      */
     rendered: string);
     /**
-     * Looks for code wrapped around ``` or ~~~. Keep in mind that if ``` is within ~~~ or vice versa, problems may occur.
+     * Looks for code wrapped around ``` or ~~~.
      *
      *
      * Can work whether or not you specificy the language after the codeing \`\`\` string. If you do not specify the language or if the language you specify is not supported/not the name highlightjs calls it by (eg: zsh is not supported but bash is), it will run hljs.highlightAuto to determine the language. This is not always accurate
